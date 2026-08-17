738a9eedd758ab882310cb31655e1faff416445d
Siddiq <syedsiddiq0703@gmail.com>
2026-08-17 23:33:04 +0530

    Add membership entity

--- src/main/java/com/syed/intellidocs/entity/Membership.java
+++ src/main/java/com/syed/intellidocs/entity/Membership.java
@@ -1,4 +1,42 @@
 package com.syed.intellidocs.entity;
 
+import com.syed.intellidocs.enums.MembershipRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = {"user_id", "organization_id"}
+                )
+        }
+)
 public class Membership {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long membershipId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MembershipRole role;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "organization_id", nullable = false)
+    private Organization organization;
+
+    @CreatedDate
+    private LocalDateTime joinedAt;
 }
