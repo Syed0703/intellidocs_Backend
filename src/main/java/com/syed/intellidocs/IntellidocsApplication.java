33921bf9d20f10260df1caae596c33a1c0e91ec5
Siddiq <syedsiddiq0703@gmail.com>
2026-08-17 14:49:49 +0530

    Initial project setup

---
+++ src/main/java/com/syed/intellidocs/IntellidocsApplication.java
@@ -0,0 +1,13 @@
+package com.syed.intellidocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellidocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellidocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syed/intellidocs/IntellidocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syed.intellidocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellidocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
