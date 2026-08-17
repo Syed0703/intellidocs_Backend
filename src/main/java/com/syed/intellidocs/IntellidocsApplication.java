446254dfa1c7a0200659e32d5ccfd048075fdd0b
Siddiq <syedsiddiq0703@gmail.com>
2026-08-17 23:36:25 +0530

    Enable Jpa Auditing

--- src/main/java/com/syed/intellidocs/IntellidocsApplication.java
+++ src/main/java/com/syed/intellidocs/IntellidocsApplication.java
@@ -2,8 +2,10 @@ package com.syed.intellidocs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IntellidocsApplication {
 
 	public static void main(String[] args) {
