a4f65bc5ea851949de7084787986a6315256956f
Siddiq <syedsiddiq0703@gmail.com>
2026-08-17 23:32:29 +0530

    Add organization entity

--- src/main/java/com/syed/intellidocs/entity/Organization.java
+++ src/main/java/com/syed/intellidocs/entity/Organization.java
@@ -1,4 +1,35 @@
 package com.syed.intellidocs.entity;
 
+import com.syed.intellidocs.enums.OrganizationStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long organizationId;
+
+    @NotBlank
+    private String organizationName;
+
+    @Enumerated(EnumType.STRING)
+    private OrganizationStatus status;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
 }
