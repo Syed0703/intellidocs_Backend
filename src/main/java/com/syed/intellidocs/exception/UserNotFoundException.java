309f5842469e7073448c3a7f8ac5a74fb80ecd00
Siddiq <syedsiddiq0703@gmail.com>
2026-08-19 23:03:59 +0530

    Create user not found exception

--- src/main/java/com/syed/intellidocs/exception/UserNotFoundException.java
+++ src/main/java/com/syed/intellidocs/exception/UserNotFoundException.java
@@ -1,4 +1,7 @@
 package com.syed.intellidocs.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException () {
+        super("User not found");
+    }
 }
