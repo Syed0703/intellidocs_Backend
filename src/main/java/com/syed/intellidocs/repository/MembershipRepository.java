b3f69f9251aad84eaf61650fead864d54778bf62
Siddiq <syedsiddiq0703@gmail.com>
2026-08-20 23:35:27 +0530

    replaced find method with exist method

--- src/main/java/com/syed/intellidocs/repository/MembershipRepository.java
+++ src/main/java/com/syed/intellidocs/repository/MembershipRepository.java
@@ -8,5 +8,9 @@ import java.util.Optional;
 
 @Repository
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
-    Optional<Membership> findByUserUserIdAndOrganizationOrganizationId(Long userId, Long organizationId);
+
+    boolean existsByUserUserIdAndOrganizationOrganizationId(
+            Long userId,
+            Long organizationId
+    );
 }
