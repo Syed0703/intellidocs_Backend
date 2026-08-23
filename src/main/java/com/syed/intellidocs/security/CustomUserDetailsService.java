e921ec921f4845bb8dbc6e1c4e53b06377cac06f
Siddiq <syedsiddiq0703@gmail.com>
2026-08-24 00:18:18 +0530

    Add custom user details service

---
+++ src/main/java/com/syed/intellidocs/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.syed.intellidocs.security;
+
+import com.syed.intellidocs.entity.User;
+import com.syed.intellidocs.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =  userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
