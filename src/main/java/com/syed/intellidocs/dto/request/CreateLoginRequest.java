98488232acbc61eff630c29103681016d9aa3bea
Siddiq <syedsiddiq0703@gmail.com>
2026-08-24 00:22:27 +0530

    Add login request dao

--- src/main/java/com/syed/intellidocs/dto/request/CreateLoginRequest.java
+++ src/main/java/com/syed/intellidocs/dto/request/CreateLoginRequest.java
@@ -1,4 +1,20 @@
 package com.syed.intellidocs.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateLoginRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
 }
