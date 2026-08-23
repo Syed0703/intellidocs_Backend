03d67b6986ea6600591f9c96ef028f5c9a735760
Siddiq <syedsiddiq0703@gmail.com>
2026-08-23 18:23:52 +0530

    Add user request dto

--- src/main/java/com/syed/intellidocs/dto/request/CreateUserRequest.java
+++ src/main/java/com/syed/intellidocs/dto/request/CreateUserRequest.java
@@ -1,4 +1,23 @@
 package com.syed.intellidocs.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateUserRequest {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
 }
