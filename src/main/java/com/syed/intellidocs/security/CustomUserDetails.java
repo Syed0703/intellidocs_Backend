ed07d0d867b819036d8908304126ea21b8db4faa
Siddiq <syedsiddiq0703@gmail.com>
2026-08-24 00:17:14 +0530

    Add custom user details

---
+++ src/main/java/com/syed/intellidocs/security/CustomUserDetails.java
@@ -0,0 +1,37 @@
+package com.syed.intellidocs.security;
+import com.syed.intellidocs.entity.User;
+import com.syed.intellidocs.enums.UserStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getStatus() == UserStatus.ACTIVE;
+    }
+}
