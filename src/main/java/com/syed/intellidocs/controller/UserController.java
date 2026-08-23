9c98e3035c60b9267ea614aa58d4b8ae0a8d97a4
Siddiq <syedsiddiq0703@gmail.com>
2026-08-23 18:25:24 +0530

    Add user controller

--- src/main/java/com/syed/intellidocs/controller/UserController.java
+++ src/main/java/com/syed/intellidocs/controller/UserController.java
@@ -1,4 +1,26 @@
 package com.syed.intellidocs.controller;
 
+import com.syed.intellidocs.dto.request.CreateUserRequest;
+import com.syed.intellidocs.dto.response.UserResponse;
+import com.syed.intellidocs.entity.User;
+import com.syed.intellidocs.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse createUser(@Valid @RequestBody CreateUserRequest request) {
+        return userService.createUser(request);
+    }
 }
