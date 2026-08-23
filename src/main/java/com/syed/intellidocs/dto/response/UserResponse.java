0eb8229a1a7274735bee194c2b54ee1ed9c92aea
Siddiq <syedsiddiq0703@gmail.com>
2026-08-23 18:24:29 +0530

    Add user response dto

--- src/main/java/com/syed/intellidocs/dto/response/UserResponse.java
+++ src/main/java/com/syed/intellidocs/dto/response/UserResponse.java
@@ -1,4 +1,14 @@
 package com.syed.intellidocs.dto.response;
 
+import com.syed.intellidocs.enums.UserStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserResponse {
+    private Long userId;
+    private String name;
+    private String email;
+    private UserStatus status;
 }
