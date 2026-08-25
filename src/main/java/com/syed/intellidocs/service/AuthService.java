db59e9c6097ab5d65156d7f416144390dab601be
Siddiq <syedsiddiq0703@gmail.com>
2026-08-25 14:59:13 +0530

    Add auth service

--- src/main/java/com/syed/intellidocs/service/AuthService.java
+++ src/main/java/com/syed/intellidocs/service/AuthService.java
@@ -1,4 +1,42 @@
 package com.syed.intellidocs.service;
 
+import com.syed.intellidocs.dto.request.CreateLoginRequest;
+import com.syed.intellidocs.dto.response.AuthResult;
+import com.syed.intellidocs.dto.response.LoginResponse;
+import com.syed.intellidocs.security.JwtService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public AuthService(AuthenticationManager authenticationManager, JwtService jwtService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+    }
+
+    public AuthResult login(CreateLoginRequest request) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+        );
+
+        Authentication authentication =  authenticationManager.authenticate(token);
+
+        String jwt = jwtService.generateToken(authentication.getName());
+
+        LoginResponse response = new LoginResponse();
+        response.setEmail(authentication.getName());
+        response.setAuthenticated(authentication.isAuthenticated());
+
+        AuthResult authResult = new AuthResult();
+        authResult.setResponse(response);
+        authResult.setToken(jwt);
+        return authResult;
+    }
 }
