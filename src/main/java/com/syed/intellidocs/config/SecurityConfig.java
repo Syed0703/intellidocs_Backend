a32fac660ab7f75f83c1be47c55dba1ff2caf96a
Siddiq <syedsiddiq0703@gmail.com>
2026-08-25 14:58:11 +0530

    Wire jwt authentication fiter and handle unauthorize request

--- src/main/java/com/syed/intellidocs/config/SecurityConfig.java
+++ src/main/java/com/syed/intellidocs/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.syed.intellidocs.config;
 import com.syed.intellidocs.security.CustomUserDetailsService;
+import com.syed.intellidocs.security.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,23 +11,42 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    public SecurityConfig(
+            CustomUserDetailsService customUserDetailsService,
+            JwtAuthenticationFilter jwtAuthenticationFilter
+    ) {
         this.customUserDetailsService = customUserDetailsService;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
-    @Bean
+    @Bean   
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users", "/api/organizations").permitAll()
+                        .requestMatchers("/api/users", "/api/auth/login")
+                        .permitAll()
                         .anyRequest().authenticated()
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(
+                                (request,
+                                 response,
+                                 authException) -> response
+                                        .sendError(HttpServletResponse.SC_UNAUTHORIZED)
+                        )
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
         return http.build();
     }
