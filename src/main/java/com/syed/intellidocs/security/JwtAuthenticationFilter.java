46662ee0e07a3012da10d644030ab8a7d7109fa9
Siddiq <syedsiddiq0703@gmail.com>
2026-08-25 14:55:54 +0530

    Add jwt authentication filter

--- src/main/java/com/syed/intellidocs/security/JwtAuthenticationFilter.java
+++ src/main/java/com/syed/intellidocs/security/JwtAuthenticationFilter.java
@@ -1,4 +1,77 @@
 package com.syed.intellidocs.security;
 
-public class JwtAuthenticationFilter {
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, CustomUserDetailsService customUserDetailsService) {
+        this.jwtService = jwtService;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        Cookie[] cookies = request.getCookies();
+
+        if(cookies == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = null;
+        for(Cookie cookie : cookies) {
+            if("access_token".equals(cookie.getName())) {
+                token = cookie.getValue();
+                break;
+            }
+        }
+
+        if(token == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            String email = jwtService.extractEmail(token);
+            if(SecurityContextHolder.getContext().getAuthentication() == null) {
+
+                UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+            filterChain.doFilter(request, response);
+
+        } catch (JwtException e) {
+            filterChain.doFilter(request, response);
+        }
+
+
+    }
 }
