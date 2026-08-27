ad290621cb0da9cb22e46996dde05ec9bfed07f0
Siddiq <syedsiddiq0703@gmail.com>
2026-08-27 23:14:47 +0530

    Add find by organization id method

--- src/main/java/com/syed/intellidocs/repository/KnowledgeBaseRepository.java
+++ src/main/java/com/syed/intellidocs/repository/KnowledgeBaseRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface KnowledgeBaseRepository extends JpaRepository<KnowledgeBase, Long> {
     boolean existsByNameAndOrganizationOrganizationId(String name, Long organizationId);
+
     List<KnowledgeBase> findByOrganizationOrganizationId(Long organizationId);
+
+    Optional<KnowledgeBase> findByKnowledgeBaseIdAndOrganizationOrganizationId(
+                Long knowledgeBaseId,
+                Long organizationId
+    );
 }
